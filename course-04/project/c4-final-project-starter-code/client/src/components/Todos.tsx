bcba8ebafd22d8cdd27628bb143262fdd63f2c17
Tuan NGUYEN <tuanshpy@gmail.com>
2022-06-05 03:33:01 +0700

    Implementing FE

--- course-04/project/c4-final-project-starter-code/client/src/components/Todos.tsx
+++ course-04/project/c4-final-project-starter-code/client/src/components/Todos.tsx
@@ -97,7 +97,11 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
         loadingTodos: false
       })
     } catch (e) {
-      alert(`Failed to fetch todos: ${e.message}`)
+      let errorMessage = "Something went wrong"
+      if (e instanceof Error) {
+        errorMessage = e.message;
+      }
+      alert(`Failed to fetch todos: ${errorMessage}`)
     }
   }
 
